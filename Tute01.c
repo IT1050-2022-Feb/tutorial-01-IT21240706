72fda4cab5cec1502c846a27b883c3cf784c817c
B.K.T.Dhananjana <99425401+IT21240706@users.noreply.github.com>
2022-02-13 09:54:42 +0000

    completed

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,20 @@
 #include <stdio.h>
 
 int main() {
+
+  int mark1, mark2, sum;
+  float average;
+
+  printf("Enter first mark : ");
+  scanf("%d", &mark1);
+
+  printf("Enter second mark : ");
+  scanf("%d", &mark2);
+
+  sum = mark1 + mark2;
+  average = sum / 2.0;
+
+  printf("Average : %.2f", average);
   
   return 0;
 }
